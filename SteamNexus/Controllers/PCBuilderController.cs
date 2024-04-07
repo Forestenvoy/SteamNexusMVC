35ef933ff4a7bdfddc1391803d95f30f9159177a
Forestenvoy <bbtsnpo@gmail.com>
2024-04-07 14:48:54 +0800

    OS Finish

--- SteamNexus/Controllers/PCBuilderController.cs
+++ SteamNexus/Controllers/PCBuilderController.cs
@@ -30,7 +30,7 @@ namespace SteamNexus.Controllers
         [HttpPost]
         public string WebScrabingTest()
         {
-            _coolPCWebScraping.UpdatePSU();
+            _coolPCWebScraping.UpdateOS();
 
             
 
