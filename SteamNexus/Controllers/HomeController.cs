7f21fd85adc6bff11d38e5fbc166f9bcbdb98c5f
Forestenvoy <bbtsnpo@gmail.com>
2024-04-08 16:38:35 +0800

    後台頁面 10%

--- SteamNexus/Controllers/HomeController.cs
+++ SteamNexus/Controllers/HomeController.cs
@@ -23,10 +23,6 @@ namespace SteamNexus.Controllers
             return View();
         }
 
-
-
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
