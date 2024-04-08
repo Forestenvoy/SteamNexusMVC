0a5e09e957daedeb661a80a6bd8cf1bf8498dbb8
Forestenvoy <bbtsnpo@gmail.com>
2024-04-08 15:14:30 +0800

    後台系統 AJAX切換PartialView 測試成功

---
+++ SteamNexus/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteamNexus.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult HardwareManagement()
+        {
+            // 
+            return PartialView("_HardwareManagementPartial");
+        }
+
+        [HttpGet]
+        public IActionResult MemberManagement()
+        {
+            // 
+            return PartialView("_MemberManagementPartial");
+        }
+        
+        [HttpGet]
+        public IActionResult GameManagement()
+        {
+            // 
+            return PartialView("_GaenManagementPartial");
+        }
+    }
+}
--- SteamNexus/Controllers/HomeController.cs
+++ SteamNexus/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace SteamNexus.Controllers
             return View();
         }
 
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
