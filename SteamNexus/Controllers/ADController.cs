d091abb243f501d6ca7893f752a6249643e60353
kkwang <kkwang7272@gmail.com>
2024-04-09 16:28:52 +0800

    新增ad後台頁面

---
+++ SteamNexus/Controllers/ADController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteamNexus.Data;
+using SteamNexus.Services;
+
+namespace SteamNexus.Controllers
+{
+    public class ADController : Controller
+    {
+        private readonly SteamNexusDbContext _context;
+
+        public ADController(SteamNexusDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
