6b3572f35eb3fb6a9f24a02dbf320730be6e3d3c
Gina628 <gina2620@gmail.com>
2024-04-10 21:39:30 +0800

    Create設定成功

--- SteamNexus/wwwroot/js/site.js
+++ SteamNexus/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function previewImage(string, img) {
+    if (string) {
+        if (string.indexOf('jpg')!==-1) {
+            img.attr("src", string);
+        }
+        else if (string.indexOf('png') !== -1) {
+            img.attr("src", string);
+        }
+        else if (string.indexOf('jpeg') !== -1) {
+            img.attr("src", string);
+        }
+        else {
+            alert("這不是圖片檔");
+        }
+        
+    }
+}
+function previewVideo(string, Video) {
+    if (string) {
+        if (string.indexOf('movie') !== -1) {
+            Video.attr("src", string);
+        }
+        else {
+            alert("這不是影片檔");
+        }
 
-// Write your JavaScript code.
+    }
+}
