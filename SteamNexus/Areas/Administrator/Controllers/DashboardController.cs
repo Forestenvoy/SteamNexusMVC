c914319485204520d195eb12a9adc191a0993955
Forestenvoy <bbtsnpo@gmail.com>
2024-04-17 14:03:54 +0800

    Area 基本設置

---
+++ SteamNexus/Areas/Administrator/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteamNexus.Areas.Administrator.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Administrator")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
