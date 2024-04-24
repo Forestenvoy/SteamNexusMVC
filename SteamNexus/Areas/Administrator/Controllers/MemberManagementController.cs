e78cf91aa042a54b5dd6b230655c7b53dbb3a4d7
ethan <sr18893828@gmail.com>
2024-04-24 14:42:58 +0800

    簡易式datatables製作完成

---
+++ SteamNexus/Areas/Administrator/Controllers/MemberManagementController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteamNexus.Data;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SteamNexus.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+
+    public class MemberManagementController : Controller
+    {
+        private ApplicationDbContext _application;
+        private readonly IWebHostEnvironment _webHost;  //上傳圖片使用
+
+        public MemberManagementController(ApplicationDbContext application, IWebHostEnvironment webHostEnvironment)
+        {
+            _application = application;
+            _webHost = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MemberManagement()
+        {
+            return PartialView("_MemberManagementPartial");
+        }
+
+        //將資料轉換成JSON檔
+        public IActionResult MemberData()
+        {
+            var result = _application.Users.Select(result => new
+            {
+                result.Name,
+                result.Email,
+                result.Gender,
+                birthday = result.Birthday.ToShortDateString(),
+                result.CPUId,
+                result.GPUId,
+                result.RAM,
+                result.Power,
+
+            });
+            return Json(result);
+
+        }
+    }
+}
