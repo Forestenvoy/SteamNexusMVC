6ced5866c2c6c2e41c6af1f05eedbcfcb8bc8cde
FUEN33 <s26@ispanaz.onmicrosoft.com>
2024-04-24 13:21:30 +0800

    廣告管理partialview json檔成功出現

---
+++ SteamNexus/Areas/Administrator/Controllers/AdvertiseManagementController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SteamNexus.Data;
+using SteamNexus.Services;
+
+namespace SteamNexus.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class AdvertiseManagementController : Controller
+    {
+        // Dependency Injection
+        private readonly SteamNexusDbContext _context;
+        // Constructor
+        public AdvertiseManagementController(SteamNexusDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: AdvertiseManagementController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult AdvertiseManagementJson()
+        {
+            return Json(_context.Advertisements);
+        }
+
+        [HttpGet]
+        public IActionResult AdvertiseManagement()
+        {
+            return PartialView("_AdvertiseManagementPartial", _context.Advertisements);
+        }
+
+        // GET: AdvertiseManagementController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdvertiseManagementController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdvertiseManagementController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdvertiseManagementController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdvertiseManagementController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdvertiseManagementController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdvertiseManagementController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
