9f89b746f2e4d149d77d7e143ceb502d4e8c34f9
Forestenvoy <bbtsnpo@gmail.com>
2024-04-27 13:06:23 +0800

    WebAPI Model環境建置，API簡易測試成功

---
+++ SteamNexusServer/Controllers/HardwareManageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SteamNexusServer.Data;
+using SteamNexusServer.Models;
+
+
+namespace SteamNexusServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HardwareManageController : ControllerBase
+    {
+        // Dependency Injection
+        private readonly SteamNexusDbContext _context;
+
+        // Constructor
+        public HardwareManageController(SteamNexusDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // 回傳硬體產品種類
+        // GET: api/HardwareManage/GetComputerParts
+        [HttpGet(Name = "GetComputerParts")]
+        public IEnumerable<string> GetComputerParts()
+        {
+            // 下拉式選單 => 硬體
+            var ComputerParts = _context.ComputerPartCategories.Select(c => c.Name);
+            return ComputerParts;
+        }
+
+    }
+}
---
+++ SteamNexusServer/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using SteamNexusServer.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DataBase Connection String
+var SteamNexusConnectionString = builder.Configuration.GetConnectionString("SteamNexus");
+// Add SteamNexusDbContext
+builder.Services.AddDbContext<SteamNexusDbContext>(options => options.UseSqlServer(SteamNexusConnectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
