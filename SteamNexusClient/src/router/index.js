880a1cb21170408aa57fd17a720b039cbe7da5b1
郭炯德 <bbtsnpo@gmail.com>
2024-04-28 12:22:50 +0800

    Vue 元件 版

---
+++ SteamNexusClient/src/router/index.js
@@ -0,0 +1,14 @@
+﻿// 引入官方元件
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 引入自定義元件
+import Hello from '@/components/HelloWorld.vue'
+
+// 啟用 VueRouter 元件
+Vue.use(VueRouter)
+
+// 匯出給 entry 使用
+export default new VueRouter({
+
+});
