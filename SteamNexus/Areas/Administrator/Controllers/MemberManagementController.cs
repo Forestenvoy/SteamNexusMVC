20f11808dad60ada6d646e6a1337144212687b9e
ethan <sr18893828@gmail.com>
2024-05-04 11:42:03 +0800

    結合User and Roles 並Json

--- SteamNexus/Areas/Administrator/Controllers/MemberManagementController.cs
+++ SteamNexus/Areas/Administrator/Controllers/MemberManagementController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SteamNexus.Data;
+using SteamNexus.Models;
 using SteamNexus.ViewModels.Game;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -30,21 +31,53 @@ namespace SteamNexus.Areas.Administrator.Controllers
             return PartialView("_MemberManagementPartial");
         }
 
-        //將資料轉換成JSON檔
+        #region JsonUser資料
         public IActionResult MemberData()
         {
             var result = _application.Users.Select(result => new
             {
                 result.Name,
                 result.Email,
-               Gender = result.Gender ? "男" : "女",
+                Gender = result.Gender ? "男" : "女",
                 birthday = result.Birthday.ToString(),
                 result.Phone,
                 result.Photo,
 
             });
             return Json(result);
+        }
+        #endregion
+
+        #region JsonRolesName
+        public JsonResult RolesData()
+        {
+            var result = _application.Roles.Select(result => new
+            {
+                result.RoleName,
+            });
+            return Json(result);
 
         }
+        #endregion
+
+        #region Json結合User and Roles
+        public IActionResult GetUsersWithRoles()
+        {
+            var result = _application.Users
+                .Join(_application.Roles, u => u.RoleId, r => r.RoleId,
+                (u, r) => new
+                {
+                    Name = u.Name,
+                    Email = u.Email,
+                    Gender = u.Gender ? "男" : "女",
+                    Birthday = u.Birthday.ToString(),
+                    Phone = u.Phone,
+                    Photo = u.Photo,
+                    RoleName = r.RoleName,
+                });
+
+            return Json(result);
+        }
+        #endregion
     }
 }
