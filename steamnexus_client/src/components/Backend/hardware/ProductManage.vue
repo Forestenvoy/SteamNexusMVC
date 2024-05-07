0363fc231a90a2067314a4dc13cd8b2efc9d36a1
郭炯德 <bbtsnpo@gmail.com>
2024-05-07 16:58:27 +0800

    用戶端初步搬運

--- steamnexus_client/src/components/Backend/hardware/ProductManage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>ProductManage</h1>
-</template>
-<script>
-export default {}
-</script>
-<style></style>
--- steamnexus_client/src/views/frontend/FrontendLayout.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button class="btn btn-primary" @click="$router.push('/admin')">後台系統</button>
-</template>
-<script>
-export default {}
-</script>
-<style></style>
