9f5354afba8367ca93f450d5442c0b162025f112
郭炯德 <bbtsnpo@gmail.com>
2024-05-12 16:15:32 +0800

    字體樣式修改

--- steamnexus_client/src/App.vue
+++ steamnexus_client/src/App.vue
@@ -45,6 +45,11 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
+// 字體變更
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap');
+$font-family-sans-serif: 'Poppins', 'Noto Sans TC';
+
 // 深色模式
 // sidebar、header、footer 背景色
 $body-bg-dark: #272829;
