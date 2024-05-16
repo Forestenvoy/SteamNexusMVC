e961c0d765f42a40082a3f7ccf0295d18345a19e
kkwang <kkwang7272@gmail.com>
2024-05-16 16:44:19 +0800

    前台廣告初版

--- SteamNexus_Client/src/views/frontend/HomePage.vue
+++ SteamNexus_Client/src/views/frontend/HomePage.vue
@@ -1,7 +1,11 @@
 <template>
+  <div>
+    <ad-carousel></ad-carousel>
+  </div>
   <div>我是首頁</div>
 </template>
-<script>
-export default {}
+<script setup>
+import AdCarousel from '@/components/frontend/home/AdCarousel.vue'
+
 </script>
 <style></style>
