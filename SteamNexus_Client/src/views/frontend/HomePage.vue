191f8518bc96d1df1577b0da9f4bf72e04adf7e7
郭炯德 <bbtsnpo@gmail.com>
2024-05-16 23:27:59 +0800

    動態 navbar 完成

--- SteamNexus_Client/src/views/frontend/HomePage.vue
+++ SteamNexus_Client/src/views/frontend/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="container p-2">
     <ad-carousel></ad-carousel>
   </div>
   <div>我是首頁</div>
+  <div style="height: 500px; width: 100px"></div>
 </template>
 <script setup>
 import AdCarousel from '@/components/frontend/home/AdCarousel.vue'
-
 </script>
 <style></style>
