b479f94c11a234305cda2f9ab588746c8fc95f4d
Forestenvoy <bbtsnpo@gmail.com>
2024-05-17 11:25:29 +0800

    硬體估價頁面 核心區塊

--- SteamNexus_Client/src/views/frontend/ComputerBuilder.vue
+++ SteamNexus_Client/src/views/frontend/ComputerBuilder.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>我是硬體估價頁面</div>
+  <!-- 硬體匹配遊戲比例 -->
+  <section class="box game-proportion"></section>
+  <!-- 菜單系統 -->
+  <section class="box menu-list">
+    <menu-carousel></menu-carousel>
+  </section>
+  <!-- 估價單系統 -->
+  <section class="box pc-builder"></section>
 </template>
-<script>
-export default {}
+
+<script setup>
+import MenuCarousel from '@/components/frontend/pcbuilder/MenuCarousel.vue'
 </script>
-<style></style>
+
+<style scoped>
+.box {
+  background-color: #eeecec;
+  border-radius: 20px;
+  border: 2px solid #525252;
+  margin-bottom: 20px;
+  padding: 20px;
+}
+
+.game-proportion {
+  width: 100%;
+  height: 500px;
+}
+
+.menu-list {
+  width: 100%;
+  height: 300px;
+}
+
+.pc-builder {
+  width: 100%;
+  height: 300px;
+}
+</style>
