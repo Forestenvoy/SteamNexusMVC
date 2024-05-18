f6e713a7ec724a51cd86f28fc9dc703d68dd29ed
ethan <sr18893828@gmail.com>
2024-05-18 15:25:22 +0800

    樣式小修

--- SteamNexus_Server/Controllers/MemberManagementController.cs
+++ SteamNexus_Server/Controllers/MemberManagementController.cs
@@ -36,7 +36,7 @@ namespace SteamNexus_Server.Controllers
 
         #region UserData
         [HttpGet("GetUsersWithRoles")]
-        [Authorize(Roles = "Member")]
+        //[Authorize(Roles = "Member")]
         //[Authorize]
         public IEnumerable<object> GetUsersWithRoles()
         {
@@ -61,7 +61,7 @@ namespace SteamNexus_Server.Controllers
 
         #region RolesData
         [HttpGet("RolesData")]
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         public IEnumerable<object> RolesData()
         {
             var result = _application.Roles.Select(result => new
@@ -455,8 +455,6 @@ namespace SteamNexus_Server.Controllers
             return BadRequest(ModelState);  // 返回表單與驗證錯誤
         }
         #endregion
-
-
 
 
         #region 密碼加密
--- SteamNexus_Server/Program.cs
+++ SteamNexus_Server/Program.cs
@@ -42,6 +42,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 #endregion
 
+
 #region JWTÅçÃÒ
 //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 //{
