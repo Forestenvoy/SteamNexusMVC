9d00184926e18ee4b4d85f38aa439ef966682a0c
郭炯德 <bbtsnpo@gmail.com>
2024-05-18 13:50:36 +0800

    AOS 引用 + 前台 layout 小修

--- steamnexus_client/src/layouts/FrontendLayout.vue
+++ steamnexus_client/src/layouts/FrontendLayout.vue
@@ -2,17 +2,14 @@
   <div>
     <div class="wrapper d-flex flex-column min-vh-100">
       <app-header></app-header>
-      <div class="body flex-grow-1">
-        <CContainer class="px-5 pt-4 position-relative" fluid>
-          <router-view></router-view>
-        </CContainer>
+      <div class="body flex-grow-1 position-relative">
+        <router-view></router-view>
       </div>
       <app-footer></app-footer>
     </div>
   </div>
 </template>
 <script setup>
-import { CContainer } from '@coreui/vue'
 import AppHeader from '@/components/frontend/share/AppHeader.vue'
 import AppFooter from '@/components/frontend/share/AppFooter.vue'
 </script>
