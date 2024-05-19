3fc9095e1fdd219a2864f85a7033e0354c846482
郭炯德 <bbtsnpo@gmail.com>
2024-05-19 12:28:58 +0800

    進度條 做了一半 ...

--- SteamNexus_Client/src/views/frontend/ComputerBuilder.vue
+++ SteamNexus_Client/src/views/frontend/ComputerBuilder.vue
@@ -1,14 +1,14 @@
 <template>
   <!-- Main Banner -->
   <builder-banner></builder-banner>
-  <!-- 硬體匹配遊戲比例 -->
-  <!-- <section class="box game-proportion" data-aos="fade-up"></section> -->
   <!-- 菜單系統 -->
   <!-- <section class="box menu-list" data-aos="fade-up">
     <menu-card-slider></menu-card-slider>
   </section> -->
   <!-- 估價單系統 -->
   <!-- <section class="box pc-builder" data-aos="fade-up"></section> -->
+  <!-- Hardware Matching Game Ratio -->
+  <game-ratio></game-ratio>
 </template>
 
 <script setup>
@@ -21,35 +21,12 @@ import { onMounted } from 'vue'
 
 // 頁面元件
 import BuilderBanner from '@/components/frontend/pcbuilder/BuilderBanner.vue'
-import MenuCardSlider from '@/components/frontend/pcbuilder/MenuCardSlider.vue'
+// import MenuCardSlider from '@/components/frontend/pcbuilder/MenuCardSlider.vue'
+import GameRatio from '@/components/frontend/pcbuilder/GameRatio.vue'
 
 onMounted(() => {
   AOS.init()
 })
 </script>
 
-<style scoped>
-.box {
-  background-color: #eeecec;
-  margin-bottom: 20px;
-  padding: 20px 20px;
-}
-
-.game-proportion {
-  width: 100%;
-  height: 500px;
-}
-
-.menu-list {
-  width: 100%;
-  height: 450px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.pc-builder {
-  width: 100%;
-  height: 300px;
-}
-</style>
+<style scoped></style>
---
+++ steamnexus_client/src/components/frontend/pcbuilder/GameRatio.vue
@@ -0,0 +1,116 @@
+<template>
+  <section class="game-ratio">
+    <CContainer>
+      <!-- 標題 -->
+      <CRow>
+        <CCol class="text-center">
+          <h1 class="title">Playable Game Ratio</h1>
+        </CCol>
+      </CRow>
+      <!-- 說明 -->
+      <CRow>
+        <CCol>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque provident quibusdam,
+            aliquam odio nostrum dolorem sint natus aliquid veritatis tempora voluptas voluptatibus
+            omnis eveniet voluptatum accusantium fugit molestias modi saepe?
+          </p>
+        </CCol>
+      </CRow>
+      <!-- 圓形進度條 -->
+      <CRow>
+        <CCol xs="12" md="6" class="ratio-box">
+          <div class="rating">
+            <h2><span class="counter" data-target="90">90</span><sup class="percent">%</sup></h2>
+            <!-- 生成進度條 -->
+            <div
+              class="block"
+              v-for="i in 100"
+              :key="i"
+              :style="`transform: rotate(${i * 3.6}deg)`"
+            ></div>
+          </div>
+        </CCol>
+        <CCol xs="12" md="6" class="ratio-box"> </CCol>
+      </CRow>
+    </CContainer>
+  </section>
+</template>
+
+<script setup>
+// vue
+import { onMounted } from 'vue'
+
+// Core UI
+import { CContainer, CRow, CCol } from '@coreui/vue'
+
+onMounted(() => {})
+</script>
+
+<style scoped>
+/* 標題 */
+.game-ratio .title {
+  font-family: 'Oswald', sans-serif;
+  font-size: calc(1.525rem + 3.3vw);
+  font-weight: 300;
+  line-height: 1.2;
+}
+
+/* 圓形進度條 */
+
+.ratio-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 200px;
+  width: 200px;
+  padding: 0;
+}
+
+.ratio-box .rating {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.ratio-box .rating .block {
+  position: absolute;
+  width: 2px;
+  height: 15px;
+  background-color: #fff;
+  left: 50%;
+  transform-origin: 50% 100px;
+  opacity: 0;
+  animation: animate 0.1s linear forwards;
+}
+
+@keyframes animate {
+  to {
+    opacity: 1;
+  }
+}
+
+.ratio-box h2 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 1.5em;
+  font-weight: 500;
+  text-align: center;
+  padding: 0;
+}
+
+.counter {
+  font-size: 2.5em;
+  font-weight: 700;
+}
+
+.percent {
+  font-size: 1.5em;
+  font-weight: 300;
+  vertical-align: middle;
+}
+</style>
