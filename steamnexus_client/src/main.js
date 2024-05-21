c615d833427a8818ec92134824cfadb26f837881
郭炯德 <bbtsnpo@gmail.com>
2024-05-21 21:28:33 +0800

    toggle 修復

--- steamnexus_client/src/main.js
+++ steamnexus_client/src/main.js
@@ -8,9 +8,6 @@ import CIcon from '@coreui/icons-vue'
 import { iconsSet as icons } from '@/assets/icons'
 
 import 'jquery'
-// import 'owl.carousel';
-// import 'owl.carousel/dist/assets/owl.carousel.css';
-// import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const app = createApp(App)
 app.use(createPinia())
