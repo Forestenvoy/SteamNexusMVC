336311961f053f6b6e72284a070434c8f1592073
郭炯德 <bbtsnpo@gmail.com>
2024-05-26 10:58:03 +0800

    進度條基本繪製

--- steamnexus_client/src/components/frontend/pcbuilder/LinearProgress.vue
+++ steamnexus_client/src/components/frontend/pcbuilder/LinearProgress.vue
@@ -1,96 +1,44 @@
 <template>
-  <div class="stepper-wrapper">
-    <div class="stepper-item completed">
-      <div class="step-counter">1</div>
-      <div class="step-name">First</div>
-    </div>
-    <div class="stepper-item completed">
-      <div class="step-counter">2</div>
-      <div class="step-name">Second</div>
-    </div>
-    <div class="stepper-item active">
-      <div class="step-counter">3</div>
-      <div class="step-name">Third</div>
-    </div>
-  </div>
+  <section class="d-flex justify-content-center">
+    <svg width="90%" height="20">
+      <!-- 繪製中間的白色虛線 -->
+      <!-- stroke-width 屬性設定了線條的粗細 -->
+      <!-- stroke-dasharray 屬性定義了線段和間隙的長度模式，第一個值表示線段長度，第二個值表示間隙長度 -->
+      <line
+        x1="10"
+        y1="10"
+        x2="100%"
+        y2="10"
+        stroke="#fff"
+        stroke-width="4"
+        stroke-dasharray="10 5"
+      />
+
+      <!-- 繪製實心的進度條部分 -->
+      <line
+        x1="10"
+        y1="10"
+        :x2="progress"
+        y2="10"
+        stroke="#f3ae0b"
+        stroke-width="4"
+        stroke-dasharray="10 5"
+      />
+    </svg>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+// vue
+import { computed } from 'vue'
+
+// pinia
+import { useBuilderStore } from '@/stores/builder.js'
+const builderStore = useBuilderStore()
+
+let progress = computed(() => `${builderStore.getCurrentStep}0%`)
+</script>
 
 <style scoped>
-.stepper-wrapper {
-  margin-top: auto;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-}
-.stepper-item {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
 
-  @media (max-width: 768px) {
-    font-size: 12px;
-  }
-}
-
-.stepper-item::before {
-  position: absolute;
-  content: '';
-  border-bottom: 2px solid #ccc;
-  width: 100%;
-  top: 20px;
-  left: -50%;
-  z-index: 2;
-}
-
-.stepper-item::after {
-  position: absolute;
-  content: '';
-  border-bottom: 2px solid #ccc;
-  width: 100%;
-  top: 20px;
-  left: 50%;
-  z-index: 2;
-}
-
-.stepper-item .step-counter {
-  position: relative;
-  z-index: 5;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background: #ccc;
-  margin-bottom: 6px;
-}
-
-.stepper-item.active {
-  font-weight: bold;
-}
-
-.stepper-item.completed .step-counter {
-  background-color: #4bb543;
-}
-
-.stepper-item.completed::after {
-  position: absolute;
-  content: '';
-  border-bottom: 2px solid #4bb543;
-  width: 100%;
-  top: 20px;
-  left: 50%;
-  z-index: 3;
-}
-
-.stepper-item:first-child::before {
-  content: none;
-}
-.stepper-item:last-child::after {
-  content: none;
-}
 </style>
