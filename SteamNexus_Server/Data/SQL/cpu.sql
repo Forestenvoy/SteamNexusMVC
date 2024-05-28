846fda9fe587ad94bcbe4e69cb0fdebd02c499c0
Forestenvoy <bbtsnpo@gmail.com>
2024-05-28 16:26:18 +0800

    GPU、RAM 資料校正 完成

--- SteamNexus_Server/Data/SQL/cpu.sql
+++ SteamNexus_Server/Data/SQL/cpu.sql
@@ -1,5 +1,5 @@
 ﻿-- 改 %XXX% 來搜尋 Id
-SELECT * FROM CPUs WHERE Name LIKE '%970%';
+SELECT * FROM CPUs WHERE Name LIKE '%2500%';
 
 -- 無法辨識的 統一用 i7-6700 
 -- CPIId 12389
--- SteamNexus_Server/Data/SQL/test.sql
+++ SteamNexus_Server/Data/SQL/test.sql
@@ -1,11 +1,9 @@
-﻿-- 抓 GPU
+﻿-- 抓 CPU
 
 -- 最低配備
--- UPDATE MinimumRequirements SET GPUId = 10412 WHERE OriGpu LIKE '%770%';
--- SELECT GPUId,OriGpu FROM MinimumRequirements WHERE OriGpu LIKE '%Iris%';
+-- UPDATE MinimumRequirements SET CPUId = 11949 WHERE OriCpu LIKE '%2500%' AND CPUId = 10000;
+SELECT CPUId,OriCpu FROM MinimumRequirements WHERE OriCpu LIKE '%2500%' AND CPUId = 10000;
 
 -- 推薦配備
--- UPDATE RecommendedRequirements SET GPUId = 10474 WHERE OriGpu LIKE '%3080%';
-SELECT GPUId,OriGpu FROM RecommendedRequirements WHERE OriGpu LIKE '%980%';
-
-
+-- UPDATE RecommendedRequirements SET CPUId = 11878 WHERE  OriCpu LIKE '%12400%' AND CPUId = 10000;
+-- SELECT CPUId,OriCpu FROM RecommendedRequirements WHERE OriCpu LIKE '%12400%' AND CPUId = 10000;
