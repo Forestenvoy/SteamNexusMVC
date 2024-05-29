a4a939b8deef26ab6cffc9b5b63acbf32d37d028
Gina628 <gina2620@gmail.com>
2024-05-29 15:20:14 +0800

    遊戲資訊修改

--- steamnexus_client/src/views/frontend/SearchSystem.vue
+++ steamnexus_client/src/views/frontend/SearchSystem.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="container p-2">
-<keyword-serch></keyword-serch>
   </div>
 </template>
 <script setup>
-import KeywordSerch from '@/components/frontend/search/KeywordSearch.vue'
+
 
 </script>
 <style scoped>
